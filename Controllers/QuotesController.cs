f0a61b439ff555ae17b4bd2ea27dae7f29ef9c5f
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-05-25 12:49:14 +0500

    default template removed and quotes controller was created

---
+++ Controllers/QuotesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quotes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuotesController : Controller
+    {
+        
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -16,17 +16,13 @@ namespace Quotes
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -34,7 +30,6 @@ namespace Quotes
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -43,13 +38,9 @@ namespace Quotes
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Quotes v1"));
             }
-
-            app.UseHttpsRedirection();
-
+            
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
